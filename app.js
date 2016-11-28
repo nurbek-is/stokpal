c5f261755681a7cedb1b6cf9db26fd7194995db0
Nurbek Ismailov <nurbek82@gmail.com>
2016-11-27 19:55:11 -0800

    added material lite styling

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
- var ref = new Firebase("https://stokpal-6159d.firebaseio.com/");
+firebase.auth().onAuthStateChanged(function(user) {
+   if (user) {
+     $(".login-cover").hide();
+   } else {
+     alert('You are not loged in');
+      var dialog = document.querySelector("loginDialog");
+      if (! dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    dialog.showModal();
+   }
+ });
