e505bcc2e9301f4c62ddddcb2c2b192a77ac2600
Nurbek Ismailov <nurbek82@gmail.com>
2016-11-29 09:26:46 -0800

    dialog /fb not working

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ firebase.auth().onAuthStateChanged(function(user) {
      $(".login-cover").hide();
    } else {
      alert('You are not loged in');
+     $(".login-cover").show();
       var dialog = document.querySelector("loginDialog");
-      if (! dialog.showModal) {
+      if (!dialog.showModal) {
       dialogPolyfill.registerDialog(dialog);
     }
     dialog.showModal();
